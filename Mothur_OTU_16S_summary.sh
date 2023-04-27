e8445c40da0df3273e99a33636824b111f85a965
liluacrobat <lilu.acrobat@gmail.com>
2023-04-27 10:22:39 -0400

    Add files via upload

---
+++ Mothur_OTU_16S_summary.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+
+biom convert -i plate_16S.OTU.org.txt -o plate_16S.OTU.biom --to-json --table-type "OTU table" --process-obs-metadata taxonomy
+
+summarize_taxa.py -i plate_16S.OTU.biom -o tax_mapping_counts/ -L 2,3,4,5,6,7 -a
+summarize_taxa.py -i plate_16S.OTU.biom -o tax_mapping_rel/ -L 2,3,4,5,6,7
+
+
