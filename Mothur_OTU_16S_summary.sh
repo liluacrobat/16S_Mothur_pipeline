a7730195eac40b5e15c18dac5b7bbf39b06628c4
liluacrobat <lilu.acrobat@gmail.com>
2024-09-20 13:50:10 -0400

    Delete Mothur_OTU_16S_summary.sh

--- Mothur_OTU_16S_summary.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-module load qiime2
-
-biom convert -i plate_16S.OTU.org.txt -o plate_16S.OTU.biom --to-json --table-type "OTU table" --process-obs-metadata taxonomy
-
-summarize_taxa.py -i plate_16S.OTU.biom -o tax_mapping_counts/ -L 2,3,4,5,6,7 -a
-summarize_taxa.py -i plate_16S.OTU.biom -o tax_mapping_rel/ -L 2,3,4,5,6,7
-
-
